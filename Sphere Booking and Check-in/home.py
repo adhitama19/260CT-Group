dcbb2c135321b10779a159b962a646672d00e50d
salahabdo <salahabdo@hotmail.co.uk>
2017-03-12 17:14:47 +0000

    Add files via upload

--- Sphere Booking and Check-in/home.py
+++ Sphere Booking and Check-in/home.py
@@ -88,28 +88,28 @@ class Login():
         self.master = master
         self.master.geometry("1080x800+200+200") # size of the window
         self.master.title("Sphere Booking and Check-in")
-        
-        self.master.username = StringVar()
-        self.master.password = StringVar()
 
+        self.conn = sqlite3.connect('Database.db')
+        self.c = self.conn.cursor()
+        
         self.label1=Label(self.master,text="Sphere Booking and Check-in",fg="black",font=("Helvetica",25))
         
         self.label2=Label(self.master,text="Username:",fg="black", font=("Helvetica",12, "bold italic"))
         self.label3=Label(self.master,text="password:",fg="black", font=("Helvetica",12, "bold italic"))
 
-        self.input1=Entry(self.master, textvariable=self.master.username)
-        self.input2=Entry(self.master, textvariable=self.master.password)
+        self.username=Entry(self.master)
+        self.password=Entry(self.master)
 
-        self.button1=Button(self.master,text="Login",fg="black",width=10, height=1,command=self.check)
+        self.button1=Button(self.master,text="Login",fg="black",width=10, height=1,command=self.checkLogin)
         self.button2=Button(self.master,text="Home",fg="black",width=10, height=1,command=self.home)
 
         self.label1.pack(side="top",padx=15,pady=15)
         
         self.label2.pack(side="top",padx=15,pady=15)
-        self.input1.pack(side="top",padx=15,pady=15)
+        self.username.pack(side="top",padx=15,pady=15)
         
         self.label3.pack(side="top",padx=15,pady=15)
-        self.input2.pack(side="top",padx=15,pady=15)
+        self.password.pack(side="top",padx=15,pady=15)
         
         self.button1.pack(side="top", padx=15,pady=15)
         self.button2.pack(side="top", padx=15,pady=15)
@@ -132,6 +132,34 @@ class Login():
         self.master.withdraw()
         admin=Admin(root3,self.master)
 
+    def closeDB(self):
+        self.c.close()
+        self.conn.close()
+        self.AdminGui()
+
+    def checkLogin(self):
+        
+        username = self.username.get()
+        password = self.password.get()
+        manager = "manager"
+
+        self.c.execute("SELECT count(1) FROM Login WHERE username = ? AND position = ?",
+                   (username, manager))
+        
+        checkUsername = self.c.fetchone()
+        cUsername = int(checkUsername[0])
+        
+        self.c.execute("SELECT count(1) FROM Login WHERE password = ? AND position = ?",
+                   (password, manager))
+
+        checkPassword = self.c.fetchone()
+        cPassword = int(checkPassword[0])
+
+        if cUsername == 1:
+            if cPassword == 1:
+                self.closeDB()
+        else:
+            print("wrong")    
 class Admin():
 
     def __init__(self,master,mainwnd):
@@ -147,7 +175,7 @@ class Admin():
         self.button1=Button(self.master,text="Add",fg="black", width=20,command=self.Add, height=3,font=("Helvetica",15, "bold italic"))
         self.button2=Button(self.master,text="Delete",fg="black", width=20, command=self.Delete, height=3,font=("Helvetica",15, "bold italic"))
         self.button3=Button(self.master,text="Update",fg="black", width=20, command=self.Update, height=3,font=("Helvetica",15, "bold italic"))
-        self.button4=Button(self.master,text="Home",fg="black", width=20, command=self.home, height=3,font=("Helvetica",15, "bold italic"))
+        self.button4=Button(self.master,text="Log out",fg="black", width=20, command=self.home, height=3,font=("Helvetica",15, "bold italic"))
         
         self.label1.pack(side="top",padx=15,pady=15)
         self.button1.pack(side="top",padx=15,pady=15)
