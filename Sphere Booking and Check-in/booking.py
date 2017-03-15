574be730bc3c5f0ae0733f6583413c432bfc0656
salahabdo <salahabdo@hotmail.co.uk>
2017-03-15 13:12:14 +0000

    Add files via upload

--- Sphere Booking and Check-in/booking.py
+++ Sphere Booking and Check-in/booking.py
@@ -4,6 +4,8 @@ Sphere Booking and Check-in
 prototype
 Salah Abdo
 Python 3
+
+This code is for the booking. 
 '''
 from Main import *
 
@@ -17,7 +19,7 @@ class Booking():
         self.master.geometry("1080x800+200+200")
         self.master.title("Sphere Booking and Check-in")
 
-        self.conn = sqlite3.connect('Database.db')
+        self.conn = sqlite3.connect('Database.db') # connects to the database
         self.c = self.conn.cursor()
 
         Label(self.master, text="Sphere Booking and Check-in",fg="black",font=("Helvetica",25)).grid(row=0,column=2)
@@ -41,18 +43,26 @@ class Booking():
         self.session = Entry(self.master)
         self.date = Entry(self.master)
         self.time = Entry(self.master)
+        # stores the entry made into a variable 
 
+        
         self.firstName.grid(row=2, column=1)
         self.surname.grid(row=4, column=1)
         self.dob.grid(row=6, column=1)
         self.session.grid(row=2, column=3)
         self.date.grid(row=4, column=3)
         self.time.grid(row=6, column=3)
+        # positioning of the entry 
 
         Button(self.master, text='Book', command=self.checkValue, font=("Helvetica",15, "bold italic")).grid(row=11, column=4, sticky=W, pady=4)
         Button(self.master, text='Timetable', command=self.table, font=("Helvetica",15, "bold italic")).grid(row=12, column=4, sticky=W, pady=4)
         Button(self.master, text='Home', command=self.goHome, font=("Helvetica",15, "bold italic")).grid(row=13, column=4, sticky=W, pady=4)
 
+        # padx = horizonta
+        # pady = verticle
+        # .grid(row=6, column=2)
+        # row = increase the number you move down, decrease the number  you move up.
+        # column = increase the number you move right, decrease the number you move left.
 
     def goHome(self):
         self.master.destroy() # close the current Member window
@@ -62,8 +72,9 @@ class Booking():
     def table(self):
         root1=Toplevel(self.master)
         timetable=timeTable(root1,self.master)
+        # Opens the slope or instructor timetable
 
-    def closeDB(self):
+    def closeDB(self): # closes the database
         self.c.close()
         self.conn.close()
         self.goHome()
@@ -78,7 +89,7 @@ class Booking():
         price = self.price
         sessionNumber = self.sessionNum
 
-        self.c.execute("INSERT INTO Booking (Ref_number, customerID, session, date, time, checking, price) VALUES(?, ?, ?, ?, ?, ?, ?)",
+        self.c.execute("INSERT INTO Booking (Ref_number, customerID, session, date, time, checking, price) VALUES(?, ?, ?, ?, ?, ?, ?)", # stores all the data into booking table 
                        (refNum, customerID, session, date, time, checking, price))                     
         
         self.c.execute("UPDATE Customer SET number_of_sessions = (?) WHERE CustomerID = (?) ",
@@ -87,7 +98,7 @@ class Booking():
         
         self.closeDB()
 
-    def id_generator(self,size=16, chars=string.ascii_uppercase + string.digits): # Creates a random 16 digit value with numbers and characters 
+    def id_generator(self,size=16, chars=string.ascii_uppercase + string.digits): # Creates a random 16 digit value with numbers and characters, used as reference number
         self.ref=''.join(random.choice(chars) for _ in range(size))
         self.insertBooking()
 
@@ -99,7 +110,7 @@ class Booking():
         instructor = 20 #price per hour
         self.price = 0
 
-        self.c.execute("SELECT Membership_Type FROM Member WHERE Customer_ID = ?",
+        self.c.execute("SELECT Membership_Type FROM Member WHERE Customer_ID = ?", # selects what membership the customer had, this will be used to calculate the price
                        (cID))
         
         self.status =self.c.fetchone()
@@ -131,16 +142,16 @@ class Booking():
         surname = self.surname.get()
         dob = self.dob.get()
 
-        # Get customer ID from the data base where the customer name, surname and date of birth matches.
-        self.c.execute("SELECT CustomerID FROM Customer WHERE FirstName = ? AND surname = ? AND date_of_birth = ?",
+        
+        self.c.execute("SELECT CustomerID FROM Customer WHERE FirstName = ? AND surname = ? AND date_of_birth = ?", # Get customer ID from the data base where the customer name, surname and date of birth matches.
                        (firstName, surname, dob))
         self.ID = self.c.fetchone()
         self.customer = str(self.ID[0])
 
-        self.c.execute("SELECT number_of_sessions FROM Customer WHERE FirstName = (?) AND surname = (?) AND date_of_birth = (?)",
+        self.c.execute("SELECT number_of_sessions FROM Customer WHERE FirstName = (?) AND surname = (?) AND date_of_birth = (?)", # Get the amount of sessions the customer has booked from the customer table
                        (firstName, surname, dob))
 
-        session = self.c.fetchone()
+        session = self.c.fetchone() 
         self.sessionNum = int(session[0])
         self.sessionNum +=1
         print(self.sessionNum)
