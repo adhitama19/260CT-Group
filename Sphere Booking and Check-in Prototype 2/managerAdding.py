5cf2cec11559f8e4ec5ac335e8f1084659046971
metodiem <kokohne8@gmail.com>
2017-04-05 01:57:38 +0100

    Add files via upload

--- Sphere Booking and Check-in Prototype 2/managerAdding.py
+++ Sphere Booking and Check-in Prototype 2/managerAdding.py
@@ -10,30 +10,28 @@ def ManagerAdd():
         global sqliteFile
         sqliteFile = 'Database.db'
         roots =Tk()
-        roots.title('Add manager')
+        roots.title('Add staff')
 
-        Title1 = Label(roots, text='Please enter manager details:')
+        Title1 = Label(roots, text='Please enter new staff details:')
         Title1.grid(row=0, column=0, sticky=E)
 
         forename = Label(roots,text='Enter Forename: ')
         surname = Label(roots,text='Enter Surename: ')
         position = Label(roots,text='Enter Position: ')
-        ID = Label(roots, text='Enter ID: ')
 
-        ID.grid(row=1, column=0, sticky=W)
-        forename.grid(row=2, column=0, sticky=W)
-        surname.grid(row=3, column=0, sticky=W)
-        position.grid(row=4, column=0, sticky=W)
+        forename.grid(row=1, column=0, sticky=W)
+        surname.grid(row=2, column=0, sticky=W)
+        position.grid(row=3, column=0, sticky=W)
 
         surnameB = Entry(roots)    
         forenameB = Entry(roots)
-        positionB = Entry(roots)
-        IDb = Entry(roots)
+        select = StringVar(roots)
+        select.set("Manager")
+        positionB = OptionMenu(roots, select, "Manager","Instructor","Slope Operator")
 
-        IDb.grid(row=1, column=1)
-        forenameB.grid(row=2, column=1)
-        surnameB.grid(row=3, column=1)
-        positionB.grid(row=4, column=1)
+        forenameB.grid(row=1, column=1)
+        surnameB.grid(row=2, column=1)
+        positionB.grid(row=3, column=1)
 
         Add = Button(roots, text='Add to database', command=AddF)
         Add.grid(row=5, column=0, sticky=W)
@@ -46,7 +44,7 @@ def AddF():
         try:
                 con = sql.connect(sqliteFile)
                 cur = con.cursor()
-                person = [(userInput1, userInput2, userInput3, userInput4)]
+                person = [(Input, userInput2, userInput3, userInput4)]
                 cur.executemany('''INSERT INTO Managers (StaffID, Forename, Surname, Position) VALUES (?,?,?,?)''', person)
                 con.commit()
 
